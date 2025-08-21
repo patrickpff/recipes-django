a42cb00e57ac8727e38fea2dcaf6c894ca6cd508
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2025-08-21 10:25:34 -0300

    models, filter by categories and others

--- recipes/models.py
+++ recipes/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
@@ -17,6 +20,9 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+
+    def __str__(self):
+        return self.title
--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from .models import Recipe
+from django.http import Http404
 
 def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
+
     return render(request, "recipes/pages/home.html", context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True
+    ).order_by('-id')
+
+    if not recipes:
+        raise Http404("Not found 😢")
+
+    return render(request, "recipes/pages/category.html", context={
+        'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category | '
     })
 
 def recipe(request, id):
