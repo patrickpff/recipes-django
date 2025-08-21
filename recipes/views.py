e317a234535af11b4d1af0b9d1ac8c7f0836c711
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2025-08-21 11:20:08 -0300

    added title on recipe detail page, added style to html recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 
@@ -25,7 +25,9 @@ def category(request, category_id):
     })
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
+
     return render(request, "recipes/pages/recipe-view.html", context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
