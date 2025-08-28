ea617684c6c0ebc512983a68d1fbe62173373409
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2025-08-28 00:12:55 -0300

    added coveragerc and test for Recipe string representation

---
+++ recipes/tests/test_recipe_model_recipe.py
@@ -0,0 +1,64 @@
+from .test_recipe_base import RecipeTestBase, Recipe
+from django.core.exceptions import ValidationError
+from parameterized import parameterized
+
+class RecipeModelTest(RecipeTestBase):
+    def setUp(self):
+        self.recipe = self.make_recipe()
+
+        return super().setUp()
+    
+    def make_recipe_no_defaults(self):
+        recipe = Recipe(
+            category=self.make_category(name="Vegan"),
+            author=self.make_author(username="janedoe"),
+            title = 'Recipe Title',
+            description = 'Recipe Description',
+            slug = 'recipe-slug',
+            preparation_time = 10,
+            preparation_time_unit = 'Minutes',
+            servings = '5',
+            servings_unit = 'Servings',
+            preparation_step = 'Recipe Preparation Steps'
+        )
+        recipe.full_clean()
+        recipe.save()
+
+        return recipe
+
+    @parameterized.expand([
+        ('title', 65),
+        ('description', 165),
+        ('preparation_time_unit', 65),
+        ('servings_unit', 65),
+    ])
+    def test_recipe_fields_max_length(self, field, max_length):
+        setattr(self.recipe, field, 'A' * (max_length + 1))
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
+
+    def test_recipe_preparation_steps_is_html_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+
+        self.assertFalse(
+            recipe.preparation_step_is_html,
+            msg='Recipe preparation_steps_is_html is not False by default.'
+        )
+
+    def test_recipe_is_published_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+
+        self.assertFalse(
+            recipe.is_published,
+            msg='Recipe preparation_steps_is_html is not False by default.'
+        )
+    
+    def test_recipe_string_representation(self):
+        self.recipe.title = "Testing representation"
+        self.recipe.full_clean()
+        self.recipe.save()
+        self.assertEqual(
+            str(self.recipe), self.recipe.title,
+            msg=f'Recipe string representation must be '
+                f'"{self.recipe.title}" but "{str(self.recipe)}" was received.'
+        )
