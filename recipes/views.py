50b33902efca4757153cbc352621df8fdd0b2594
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2025-09-01 16:01:44 -0300

    styling messages and creation of authors app

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-from utils.recipes.factory import make_recipe
 from .models import Recipe
 from django.http import Http404
 from django.db.models import Q
-from django.core.paginator import Paginator
 from utils.pagination import make_pagination
 import os
 
