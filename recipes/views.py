356264e9e5aa05295b86ca859cc513e98ad1bb81
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2025-09-01 12:08:34 -0300

    paginate recipes

--- recipes/views.py
+++ recipes/views.py
@@ -3,14 +3,19 @@ from utils.recipes.factory import make_recipe
 from .models import Recipe
 from django.http import Http404
 from django.db.models import Q
+from django.core.paginator import Paginator
+from utils.pagination import make_pagination
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
+    
+    page_obj, pagination_range = make_pagination(request=request, queryset=recipes, per_page=9)
 
     return render(request, "recipes/pages/home.html", context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range
     })
 
 def category(request, category_id):
@@ -20,9 +25,12 @@ def category(request, category_id):
             is_published=True
         ).order_by('-id')
     )
+    
+    page_obj, pagination_range = make_pagination(request=request, queryset=recipes, per_page=9)
 
     return render(request, "recipes/pages/category.html", context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
         'title': f'{recipes[0].category.name} - Category | '
     })
 
@@ -48,9 +56,13 @@ def search(request):
         is_published=True
     )
     recipes = recipes.order_by('-id')
+
+    page_obj, pagination_range = make_pagination(request=request, queryset=recipes, per_page=9)
     
     return render(request, "recipes/pages/search.html", context={
         'page_title': f'Search for "{search_term}"',
         'search_term': search_term,
-        'recipes': recipes
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
+        'additional_url_query': f'&q={search_term}',
     })
