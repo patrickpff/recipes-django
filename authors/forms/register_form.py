3e51e40d9eea51e48e80b933959c68c9c4773d63
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2025-09-16 23:48:17 -0300

    login form and view

--- authors/forms/register_form.py
+++ authors/forms/register_form.py
@@ -1,21 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-import re
-
-def strong_password(password):
-    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
-
-    if not regex.match(password):
-        raise ValidationError(
-            (
-                "Password must have at least one uppercase letter, "
-                "one lowercase letter and one number. The length should be "
-                "at least 8 characters long."
-            ),
-            code='Invalid'
-        )
-
+from utils.django_forms import strong_password
 
 class RegisterForm(forms.ModelForm):
     password = forms.CharField(
--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 from django.http import Http404
 from django.contrib import messages
 from django.urls import reverse
@@ -10,7 +10,7 @@ def register_view(request):
 
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
-        'form_action': reverse('authors:create')
+        'form_action': reverse('authors:register_create')
     })
 
 def register_create(request):
@@ -31,3 +31,13 @@ def register_create(request):
         del(request.session['register_form_data'])
     
     return redirect('authors:register')
+
+def login_view(request):
+    form = LoginForm()
+    return render(request, 'authors/pages/login_view.html', {
+        'form': form,
+        'form_action': reverse('authors:login_create')
+    })
+
+def login_create(request):
+    return render(request, 'authors/pages/login_view.html')
