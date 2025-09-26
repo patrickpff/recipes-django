9da407aed097124c98c931d3bd75e5353d4e3473
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2025-09-26 10:49:15 -0300

    redirect to new dashboard page after login

--- authors/views.py
+++ authors/views.py
@@ -54,7 +54,6 @@ def login_create(request):
         raise Http404()
 
     form = LoginForm(request.POST)
-    login_url = reverse('authors:login')
 
     if form.is_valid():
         authenticated_user = authenticate(
@@ -73,7 +72,7 @@ def login_create(request):
     else:
         messages.error(request, 'Invalid username or password.')
 
-    return redirect(login_url)
+    return redirect(reverse('authors:dashboard'))
 
 
 @login_required(login_url='authors:login', redirect_field_name='next')
@@ -90,3 +89,8 @@ def logout_view(request):
     logout(request)
 
     return redirect(reverse('authors:login'))
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard(request):
+    return render(request, 'authors/pages/dashboard.html')
