e8211b29138e269fc98d18379da2f7e5f07db895
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2025-10-15 16:30:02 -0300

    creating APIs with django

--- recipes/urls.py
+++ recipes/urls.py
@@ -17,4 +17,14 @@ urlpatterns = [
         name="category"
     ),
     path('recipes/<int:pk>/',  views.RecipeDetail.as_view(), name="recipe"),
+    path(
+        'recipes/api/v1/',
+        views.RecipeListViewHomeApi.as_view(),
+        name="home_api_v1"
+    ),
+    path(
+        'recipes/api/v1/<int:pk>/',
+        views.RecipeDetailAPI.as_view(),
+        name="recipes_api_v1_detail"
+    ),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,8 @@
 import os
 
 from django.db.models import Q
-from django.http import Http404
+from django.forms.models import model_to_dict
+from django.http import Http404, JsonResponse
 from django.views.generic import DetailView, ListView
 
 from utils.pagination import make_pagination
@@ -43,6 +44,19 @@ class RecipeListViewBase(ListView):
 
 class RecipeListViewHome(RecipeListViewBase):
     template_name = 'recipes/pages/home.html'
+
+
+class RecipeListViewHomeApi(RecipeListViewBase):
+    template_name = 'recipes/pages/home.html'
+
+    def render_to_response(self, context, **response_kwargs):
+        recipes = self.get_context_data()['recipes'].object_list.values()
+
+        return JsonResponse(
+            {
+                "recipes": list(recipes)
+            }
+        )
 
 
 class RecipeListViewCategory(RecipeListViewBase):
@@ -119,3 +133,27 @@ class RecipeDetail(DetailView):
         })
 
         return ctx
+
+
+class RecipeDetailAPI(RecipeDetail):
+    def render_to_response(self, context, **response_kwargs):
+        recipe = self.get_context_data()['recipe']
+        recipe_dict = model_to_dict(recipe)
+
+        recipe_dict['created_at'] = str(recipe.created_at)
+        recipe_dict['updated_at'] = str(recipe.created_at)
+
+        if recipe_dict.get('cover'):
+            recipe_dict['cover'] = self.request.build_absolute_uri(
+                recipe_dict['cover'].url
+            )
+        else:
+            recipe_dict['cover'] = ''
+
+        del recipe_dict['is_published']
+        del recipe_dict['preparation_step_is_html']
+
+        return JsonResponse(
+            recipe_dict,
+            safe=False
+        )
