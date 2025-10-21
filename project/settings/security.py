bca05525db2a9f8dc089d0b1c87b3a0d9a03e963
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2025-10-21 19:08:18 -0300

    separating settings.py into a package

---
+++ project/settings/security.py
@@ -0,0 +1,17 @@
+# Password validation
+# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', # noqa
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', # noqa
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', # noqa
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', # noqa
+    },
+]
