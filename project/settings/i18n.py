c02af5c8ba395c4c863cb24428441fd9bf78ba85
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2025-10-22 17:29:40 -0300

    added translation

--- project/settings/i18n.py
+++ project/settings/i18n.py
@@ -1,5 +1,6 @@
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
+from . import BASE_DIR
 
 LANGUAGE_CODE = 'en-us'
 
@@ -8,3 +9,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+]
