1d909854da621b0e386e9e88a790c65c452c4e62
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2025-10-22 15:54:22 -0300

    creating url and template for profile

--- authors/models.py
+++ authors/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(default='', blank=True)
+    photo = models.ImageField(
+        upload_to='authors/profile/%Y/%m/%d/',
+        blank=True,
+        default=''
+    )
--- recipes/models.py
+++ recipes/models.py
@@ -12,6 +12,9 @@ from tag.models import Tag
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    class Meta:
+        verbose_name_plural = "categories"
+
     def __str__(self):
         return self.name
 
