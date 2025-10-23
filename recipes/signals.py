dcfd55a9fe3b72b29fd904da8bd48cc355ff8eff
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2025-10-23 10:52:28 -0300

    fixing signals and installing djangorestframework

--- recipes/signals.py
+++ recipes/signals.py
@@ -15,14 +15,16 @@ def delete_cover(instance):
 
 @receiver(pre_delete, sender=Recipe)
 def recipe_cover_delete(sender, instance, *args, **kwargs):
-    old_instance = Recipe.objects.filter(pk=instance.pk).get()
-    delete_cover(old_instance)
+    old_instance = Recipe.objects.filter(pk=instance.pk).first()
+
+    if old_instance:
+        delete_cover(old_instance)
 
 
 @receiver(pre_save, sender=Recipe)
 def recipe_cover_update(sender, instance, raw, *args, **kwargs):
     if (instance.pk):
-        old_instance = Recipe.objects.filter(pk=instance.pk).get()
+        old_instance = Recipe.objects.filter(pk=instance.pk).first()
 
         if old_instance.cover != instance.cover:
             delete_cover(old_instance)
