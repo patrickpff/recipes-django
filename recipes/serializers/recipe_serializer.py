2b57e730855e736ab028bf96dead36b48d81ffd6
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2025-10-23 19:33:41 -0300

    get related fields

--- recipes/serializers/recipe_serializer.py
+++ recipes/serializers/recipe_serializer.py
@@ -1,4 +1,13 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
+
+from tag.models import Tag
+
+
+class TagSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField(max_length=65)
+    slug = serializers.SlugField()
 
 
 class RecipeSerializer(serializers.Serializer):
@@ -7,6 +16,17 @@ class RecipeSerializer(serializers.Serializer):
     description = serializers.CharField(max_length=65)
     public = serializers.BooleanField(source="is_published")
     preparation = serializers.SerializerMethodField()
+    category = serializers.StringRelatedField()
+    author = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all()
+    )
+    tags = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.all(),
+        many=True
+    )
+    tag_objects = TagSerializer(
+        many=True, source='tags'
+    )
 
     def get_preparation(self, recipe):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
