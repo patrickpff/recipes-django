b8273ef06dc5dd281656ce182b81a105c59e3b9e
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2025-10-30 17:29:56 -0300

    adding author to recipe api on create

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
@@ -21,6 +22,17 @@ class RecipeApiV2ViewSet(ModelViewSet):
     serializer_class = RecipeSerializer
     pagination_class = RecipeApiV2Pagination
     permission_classes = [IsAuthenticatedOrReadOnly, ]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
 
     def get_queryset(self):
         qs = super().get_queryset()
