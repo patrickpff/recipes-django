3de0bcfe5341ca0bc47123860c062090d0ade3a0
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2025-10-30 17:19:13 -0300

    added permissions to create, update and delete recipes

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,13 +1,14 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from tag.models import Tag
 
 from ..models import Recipe
+from ..permissions import IsOwner
 from ..serializers import RecipeSerializer, TagSerializer
 
 
@@ -19,7 +20,7 @@ class RecipeApiV2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeApiV2Pagination
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -29,6 +30,38 @@ class RecipeApiV2ViewSet(ModelViewSet):
             qs = qs.filter(category_id=category_id)
 
         return qs
+
+    def get_object(self):
+        pk = self.kwargs.get('pk')
+        obj = get_object_or_404(
+            self.get_queryset(),
+            pk=pk,
+        )
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsOwner(), ]
+
+        return super().get_permissions()
+
+    def partial_update(self, request, *args, **kwargs):
+        recipe = self.get_object()
+        serializer = RecipeSerializer(
+            instance=recipe,
+            data=request.data,
+            many=False,
+            context={'request': request},
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(
+            serializer.data,
+        )
 
 
 @api_view()
