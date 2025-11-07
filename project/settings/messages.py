4f85743e831b25b889b09ac7b7d14876733755ed
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2025-11-07 08:56:57 -0300

    alteration in messages.py

--- project/settings/messages.py
+++ project/settings/messages.py
@@ -1,9 +1,9 @@
 from django.contrib.messages import constants
 
 MESSAGE_TAGS = {
-    constants.DEBUG: 'message-debug',
-    constants.ERROR: 'message-error',
-    constants.INFO: 'message-info',
-    constants.SUCCESS: 'message-success',
-    constants.WARNING: 'message-warning',
+    constants.DEBUG: "message-debug",
+    constants.ERROR: "message-error",
+    constants.INFO: "message-info",
+    constants.SUCCESS: "message-success",
+    constants.WARNING: "message-warning",
 }
